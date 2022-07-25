82bc66c0c834524d7159799515bb1cee6e5e2213
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-25 20:43:41 +0200

    Collegato correttamente ogni vocale alla giusta voce delle opzioni

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,19 +13,21 @@ voiceList = document.querySelector("select")
 console.log(voiceList);
 
 /* #4.1 - salvo "speechSynthesis" in una variabile */
-let synthesis = speechSynthesis;
+let synth = speechSynthesis;
 
 /* #4.2 - Avvio la creazione della funzione legata alle opzioni disponibili */
 function voices() {
-    for (let voice of synthesis.getVoices()) {
+    for (let voice of synth.getVoices()) {
+        // #4.6 - Seleziono di default la voce in inglese
+        let selected = voice.name === "Google US English" ? "selected" : ""
         /* console.log(voice); */ // Verifico in console log tutta la lista di traduttori vocali disponibili
-        let option = `<option value="${voice.name}">${voice.name} (${voice.lang})</option>` // #4.4 - Salvo le opzioni di traduzione plausibili all'interno dei miei option
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>` // #4.4 - Salvo le opzioni di traduzione plausibili all'interno dei miei option
         voiceList.insertAdjacentHTML("beforeend", option) // #4.5 - Inserisco dinamicamente i miei tag option all'interno di select
     }
 }
 
 /* #4.3 - Ora scatendando l'evento "voiceschanged" posso prendere tutti i traduttori vocali disponibili */
-synthesis.addEventListener("voiceschanged", voices)
+synth.addEventListener("voiceschanged", voices)
 
 
 
@@ -33,7 +35,15 @@ synthesis.addEventListener("voiceschanged", voices)
 function textToSpeech(text) {
     /* #3.1 - Creo una variabile (utterance) che, come valore ha la funzione di vocalizzatore che possiede come parametro Text (sarà il mio textArea.value)  */
     let utterance = new SpeechSynthesisUtterance(text) // Questo è il mio testo che viene salvato in una variabile
-    speechSynthesis.speak(utterance) // #3.2 - Vocalizzatore vero e proprio
+    /* #5 - Avvio nuovo ciclo per dare a ogni option il suo corretto valore */
+    for (let voice of synth.getVoices()) {
+        // SE voice.name ha un riscontro con un valore presente in voiceList
+        if (voice.name === voiceList.value) {
+            /* Allora la mia utterance.voice Equivale al parametro voice relativo a quel vocale */
+            utterance.voice = voice
+        }
+    }
+    synth.speak(utterance) // #3.2 - Vocalizzatore vero e proprio
 }
 
 /* #2.2 - Scateno un evento sul mio button */
